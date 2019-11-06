cd1d1858d9c0bf7134ad7998fe43502d72101468
DerModMaster <dermodmaster@googlemail.com>
2019-11-06 13:53:38 +0100

    unhandle exception

--- src/ChatServerImpl.java
+++ src/ChatServerImpl.java
@@ -2,6 +2,10 @@ import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ChatServerImpl extends UnicastRemoteObject implements ChatServer {
+    protected ChatServerImpl() throws RemoteException {
+
+    }
+
     @Override
     public ChatProxy subscribeUser(ClientProxy handle) throws RemoteException {
         return null;
