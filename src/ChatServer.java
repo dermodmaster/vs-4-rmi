2c5541da2fb2f9bcb03a5583cf824da365231a5a
DerModMaster <dermodmaster@googlemail.com>
2019-11-06 17:53:07 +0100

    adding sendMessage to server interface

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -4,4 +4,6 @@ public interface ChatServer extends Remote {
     public ChatProxy subscribeUser(ClientProxy handle) throws RemoteException;
 
     public boolean unsubscribeUser(ClientProxy handle) throws RemoteException;
+
+    void sendMessage(String username, String message);
 }
