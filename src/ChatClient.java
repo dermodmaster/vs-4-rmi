fe9f4821a35b199033bfe331cb3629e398fa1313
DerModMaster <dermodmaster@googlemail.com>
2019-11-06 15:11:32 +0100

    impl

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,8 +1,11 @@
+import javax.swing.*;
 import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class ChatClient {
+public class ChatClient extends JFrame {
+
+
     public static void main (String[] args){
         try{
             Registry registry = LocateRegistry.getRegistry();
--- src/ClientProxyImpl.java
+++ src/ClientProxyImpl.java
@@ -1,6 +1,12 @@
 import java.rmi.RemoteException;
 
 public class ClientProxyImpl implements ClientProxy {
+    ChatClient client;
+
+    public ClientProxyImpl(ChatClient client) throws RemoteException {
+        this.client = client;
+    }
+
     public void receiveMessage(String username, String message) throws RemoteException{
 
     }
